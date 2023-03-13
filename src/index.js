f3ffbf10b56261684330c73b973ff0c79820df8a
elad237 <tumbu.elad@gmail.com>
2023-03-13 14:01:00 +0100

    Added the Boostrap folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
